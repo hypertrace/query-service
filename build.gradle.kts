23af43c6f43ff79b0e77b6e26453ab599ddbca49
Ronak <ronak@traceable.ai>
2020-07-12 16:21:38 +0530

    Updates to community license

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("org.hypertrace.repository-plugin") version "0.1.2"
   id("org.hypertrace.ci-utils-plugin") version "0.1.1"
-  id("org.hypertrace.publish-plugin") version "0.1.5" apply false
+  id("org.hypertrace.publish-plugin") version "0.3.0" apply false
   id("org.hypertrace.jacoco-report-plugin") version "0.1.0" apply false
 }
 
@@ -9,7 +9,7 @@ subprojects {
   group = "org.hypertrace.core.query.service"
   pluginManager.withPlugin("org.hypertrace.publish-plugin") {
     configure<org.hypertrace.gradle.publishing.HypertracePublishExtension> {
-      license.set(org.hypertrace.gradle.publishing.License.AGPL_V3)
+      license.set(org.hypertrace.gradle.publishing.License.TRACEABLE_COMMUNITY)
     }
   }
 }
