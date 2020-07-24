f4ca10155e038daa1de98d14ae2c21102169ec4b
Mohit Agarwal <mohit@traceable.ai>
2020-07-24 14:23:42 +0530

    Update to 0.1.3

--- query-service/build.gradle.kts
+++ query-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 dependencies {
   implementation(project(":query-service-impl"))
-  implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.1.2")
+  implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.1.3")
   implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.2")
   implementation("io.grpc:grpc-netty:1.30.2")
 
