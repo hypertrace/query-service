9a2e0f6db32173243d098f4eb0b0305eaf69ce13
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2020-10-30 11:53:36 -0700

    fix: update pinot conversion for concat operator (#33)

--- query-service-impl/src/test/java/org/hypertrace/core/query/service/pinot/converters/PinotFunctionConverterTest.java
+++ query-service-impl/src/test/java/org/hypertrace/core/query/service/pinot/converters/PinotFunctionConverterTest.java
@@ -1,6 +1,7 @@
 package org.hypertrace.core.query.service.pinot.converters;
 
 import static org.hypertrace.core.query.service.QueryFunctionConstants.QUERY_FUNCTION_AVG;
+import static org.hypertrace.core.query.service.QueryFunctionConstants.QUERY_FUNCTION_CONCAT;
 import static org.hypertrace.core.query.service.QueryFunctionConstants.QUERY_FUNCTION_COUNT;
 import static org.hypertrace.core.query.service.QueryFunctionConstants.QUERY_FUNCTION_DISTINCTCOUNT;
 import static org.hypertrace.core.query.service.QueryFunctionConstants.QUERY_FUNCTION_MAX;
@@ -10,6 +11,7 @@ import static org.hypertrace.core.query.service.QueryFunctionConstants.QUERY_FUN
 import static org.hypertrace.core.query.service.QueryRequestBuilderUtils.createColumnExpression;
 import static org.hypertrace.core.query.service.QueryRequestBuilderUtils.createIntLiteralValueExpression;
 import static org.hypertrace.core.query.service.QueryRequestBuilderUtils.createLongLiteralValueExpression;
+import static org.hypertrace.core.query.service.QueryRequestBuilderUtils.createStringLiteralValueExpression;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -158,6 +160,23 @@ class PinotFunctionConverterTest {
         new PinotFunctionConverter()
             .convert(
                 buildFunction("UNKNOWN", createColumnExpression("foo")),
+                this.mockArgumentConverter));
+  }
+
+  @Test
+  void convertsConcatFunctionAddingSeparator() {
+    Expression column1 = createColumnExpression("foo").build();
+    Expression column2 = createColumnExpression("bar").build();
+    Expression separator = createStringLiteralValueExpression("");
+
+    when(this.mockArgumentConverter.apply(column1)).thenReturn("foo");
+    when(this.mockArgumentConverter.apply(column2)).thenReturn("bar");
+    when(this.mockArgumentConverter.apply(separator)).thenReturn("''");
+    assertEquals(
+        "CONCAT(foo,bar,'')",
+        new PinotFunctionConverter()
+            .convert(
+                buildFunction(QUERY_FUNCTION_CONCAT, column1.toBuilder(), column2.toBuilder()),
                 this.mockArgumentConverter));
   }
 
