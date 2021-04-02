d7692e86139e4fe0bde0c10dfbc17ef2f9b1a6da
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2021-04-02 08:32:50 -0700

    chore: update vulnerabilities (#63)
    * chore: update vulnerabilities
    
    * chore: move netty to runtime only, dedupe config

--- query-service-impl/build.gradle.kts
+++ query-service-impl/build.gradle.kts
@@ -10,55 +10,46 @@ tasks.test {
 
 dependencies {
   constraints {
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.0") {
-      because("Deserialization of Untrusted Data [High Severity][https://snyk.io/vuln/SNYK-JAVA-COMFASTERXMLJACKSONCORE-561587] in com.fasterxml.jackson.core:jackson-databind@2.9.8\n" +
-          "   used by org.apache.pinot:pinot-java-client")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.2") {
+      because("Multiple vulnerabilities")
     }
     implementation("io.netty:netty:3.10.6.Final") {
-      because("HTTP Request Smuggling [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-473694] in io.netty:netty@3.9.6.Final\n" +
-          "    introduced by org.apache.pinot:pinot-java-client")
+      because("https://snyk.io/vuln/SNYK-JAVA-IONETTY-30430")
     }
     implementation("org.apache.zookeeper:zookeeper:3.6.2") {
-      because("Authentication Bypass [High Severity][https://snyk.io/vuln/SNYK-JAVA-ORGAPACHEZOOKEEPER-32301] in org.apache.zookeeper:zookeeper@3.4.6\n" +
-          "    introduced by org.apache.pinot:pinot-java-client")
+      because("Multiple vulnerabilities")
     }
-    implementation("commons-codec:commons-codec:1.13") {
-      because("Information Exposure [Low Severity][https://snyk.io/vuln/SNYK-JAVA-COMMONSCODEC-561518] in commons-codec:commons-codec@1.11"
-          + " introduced org.apache.httpcomponents:httpclient@4.5.12")
+    implementation("io.netty:netty-transport-native-epoll:4.1.63.Final") {
+      because("Multiple vulnerabilities")
     }
-    implementation("io.netty:netty-transport-native-epoll:4.1.59.Final") {
-      because("Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082238] in io.netty:netty-transport-native-epoll@4.1.50.Final\n" +
-              "    introduced by io.netty:netty-transport-native-epoll@4.1.50.Final")
-    }
-    implementation("io.netty:netty-handler:4.1.59.Final") {
-      because("Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082235] in io.netty:netty-handler@4.1.50.Final\n" +
-              "    introduced by io.netty:netty-handler@4.1.50.Final")
+    implementation("io.netty:netty-handler:4.1.63.Final") {
+      because("Multiple vulnerabilities")
     }
   }
   api(project(":query-service-api"))
-  api("com.typesafe:config:1.4.0")
-  implementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.3.3")
-  implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.3.3")
-  implementation("org.hypertrace.core.grpcutils:grpc-server-rx-utils:0.3.3")
-  implementation("org.hypertrace.core.attribute.service:attribute-service-api:0.8.7")
-  implementation("org.hypertrace.core.attribute.service:attribute-projection-registry:0.8.7")
-  implementation("org.hypertrace.core.attribute.service:caching-attribute-service-client:0.8.7")
-  implementation("org.hypertrace.core.serviceframework:service-framework-spi:0.1.21")
-  implementation("com.google.inject:guice:4.2.3")
+  api("com.typesafe:config:1.4.1")
+  implementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.4.0")
+  implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.4.0")
+  implementation("org.hypertrace.core.grpcutils:grpc-server-rx-utils:0.4.0")
+  implementation("org.hypertrace.core.attribute.service:attribute-service-api:0.10.5")
+  implementation("org.hypertrace.core.attribute.service:attribute-projection-registry:0.10.5")
+  implementation("org.hypertrace.core.attribute.service:caching-attribute-service-client:0.10.5")
+  implementation("org.hypertrace.core.serviceframework:service-framework-spi:0.1.23")
+  implementation("com.google.inject:guice:5.0.1")
   implementation("org.apache.pinot:pinot-java-client:0.6.0") {
     // We want to use log4j2 impl so exclude the log4j binding of slf4j
     exclude("org.slf4j", "slf4j-log4j12")
   }
   implementation("org.slf4j:slf4j-api:1.7.30")
-  implementation("commons-codec:commons-codec:1.13")
-  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.21")
-  implementation("com.google.protobuf:protobuf-java-util:3.12.2")
-  implementation("com.google.guava:guava:30.0-jre")
-  implementation("io.reactivex.rxjava3:rxjava:3.0.6")
+  implementation("commons-codec:commons-codec:1.15")
+  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.23")
+  implementation("com.google.protobuf:protobuf-java-util:3.15.6")
+  implementation("com.google.guava:guava:30.1.1-jre")
+  implementation("io.reactivex.rxjava3:rxjava:3.0.11")
 
   testImplementation(project(":query-service-api"))
-  testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
-  testImplementation("org.mockito:mockito-core:3.3.3")
-  testImplementation("org.mockito:mockito-junit-jupiter:3.3.3")
-  testImplementation("org.apache.logging.log4j:log4j-slf4j-impl:2.13.3")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+  testImplementation("org.mockito:mockito-core:3.8.0")
+  testImplementation("org.mockito:mockito-junit-jupiter:3.8.0")
+  testImplementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
 }
