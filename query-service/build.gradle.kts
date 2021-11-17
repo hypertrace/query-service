497ba266e87c67dca2440cfa20ffc0ddeec1dbb6
Sarthak Singhal <sarthaksinghal@Sarthaks-MacBook-Pro.local>
2021-11-17 21:14:56 +0530

    updated grpc version

--- query-service/build.gradle.kts
+++ query-service/build.gradle.kts
@@ -15,13 +15,8 @@ dependencies {
   implementation("org.slf4j:slf4j-api:1.7.30")
   implementation("com.typesafe:config:1.4.1")
 
-  constraints {
-    implementation("com.google.code.gson:gson:2.8.9") {
-      because("https://snyk.io/vuln/SNYK-JAVA-COMGOOGLECODEGSON-1730327")
-    }
-  }
   runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
-  runtimeOnly("io.grpc:grpc-netty:1.36.1")
+  runtimeOnly("io.grpc:grpc-netty:1.42.0")
   constraints {
     runtimeOnly("io.netty:netty-codec-http2:4.1.68.Final") {
       because("https://snyk.io/vuln/SNYK-JAVA-IONETTY-1089809")
