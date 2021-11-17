cc5cdffadb237245e01942d368cd191713bc774d
sarthak <sarthak02singhal@gmail.com>
2021-11-17 21:30:38 +0530

    Updated grpc versions (#101)
    * updated grpc version
    
    * removed changes from diff branch
    
    * updated grpc versions
    
    Co-authored-by: Sarthak Singhal <sarthaksinghal@Sarthaks-MacBook-Pro.local>

--- query-service-api/build.gradle.kts
+++ query-service-api/build.gradle.kts
@@ -23,7 +23,7 @@ protobuf {
     // the identifier, which can be referred to in the "plugins"
     // container of the "generateProtoTasks" closure.
     id("grpc_java") {
-      artifact = "io.grpc:protoc-gen-grpc-java:1.36.1"
+      artifact = "io.grpc:protoc-gen-grpc-java:1.42.0"
     }
 
     if (generateLocalGoGrpcFiles) {
@@ -66,15 +66,10 @@ tasks.test {
 }
 
 dependencies {
-  api("io.grpc:grpc-protobuf:1.36.1")
-  api("io.grpc:grpc-stub:1.36.1")
+  api("io.grpc:grpc-protobuf:1.42.0")
+  api("io.grpc:grpc-stub:1.42.0")
   api("javax.annotation:javax.annotation-api:1.3.2")
 
-  constraints {
-    implementation("com.google.code.gson:gson:2.8.9") {
-      because("https://snyk.io/vuln/SNYK-JAVA-COMGOOGLECODEGSON-1730327")
-    }
-  }
   testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
   testImplementation("com.google.protobuf:protobuf-java-util:3.15.6")
 }
