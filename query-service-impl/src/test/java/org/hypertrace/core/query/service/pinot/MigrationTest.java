b18a7b9095290de366deba40d1a6c6f20ba5e63e
Sarthak Singhal <sarthaksinghal@Sarthaks-MacBook-Pro.local>
2021-12-02 18:19:51 +0530

    added unit test

--- query-service-impl/src/test/java/org/hypertrace/core/query/service/pinot/MigrationTest.java
+++ query-service-impl/src/test/java/org/hypertrace/core/query/service/pinot/MigrationTest.java
@@ -107,6 +107,25 @@ public class MigrationTest {
   }
 
   @Test
+  public void testQuerySelectionUsingMapAttributeWithSubPathWithColumnAttribute() {
+    Builder builder = QueryRequest.newBuilder();
+    builder.addSelection(createComplexAttributeExpression("Span.id", "span.kind"));
+    ViewDefinition viewDefinition = getDefaultViewDefinition();
+    defaultMockingForExecutionContext();
+
+    assertPQLQuery(
+        builder.build(),
+        "Select span_id FROM SpanEventView "
+            + "where "
+            + viewDefinition.getTenantIdColumn()
+            + " = '"
+            + TENANT_ID
+            + "'",
+        viewDefinition,
+        executionContext);
+  }
+
+  @Test
   public void testQuerySelectionUsingMapAttributeWithoutSubPath() {
     Builder builder = QueryRequest.newBuilder();
     builder.addSelection(createSimpleAttributeExpression("Span.tags"));
