eae5faee6a7dfce101605193227f901a5e6dcc39
Ronak <ronak@traceable.ai>
2021-12-09 17:10:58 +0530

    wip for building time series response

--- query-service-impl/src/main/java/org/hypertrace/core/query/service/prometheus/PrometheusBasedResponseBuilder.java
+++ query-service-impl/src/main/java/org/hypertrace/core/query/service/prometheus/PrometheusBasedResponseBuilder.java
@@ -1,8 +1,10 @@
 package org.hypertrace.core.query.service.prometheus;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import okhttp3.Request;
 import org.hypertrace.core.query.service.api.Row;
 import org.hypertrace.core.query.service.api.Row.Builder;
@@ -15,32 +17,43 @@ public class PrometheusBasedResponseBuilder {
    * PromQL response
    * Map<columnName, metrics_attribute> (SERVICE.id, service_id)
    * Map<columnName, query_name> (SERVICE.numCalls, query) -> merticMap
-   * selectionList : list of all selected columns request metatdata
+   * columnSet : list of all selected columns request metatdata
    * <SERVICE.startTime, SERVICE.id, SERVICE.numCall, Service.errorCount> // columnSet
    * timeStampColumn : SERVICE.startTime
    * */
-  public static void buildQSResponse(
+  static void buildResponse(
       Map<Request, PromQLMetricResponse> promQLMetricResponseMap,
       Map<String, String> columnNameToAttributeMap,
       Map<String, String> columnNameToQueryMap,
-      List<String> selectionList) {
+      List<String> columnSet,
+      String timeStampColumn) {
 
+    // check if response is empty
     if (promQLMetricResponseMap.isEmpty()) {
       return;
     }
 
+    // as multiple request only vary in metric value, and all other attributes
+    // and number of rows are same, we can use one of the metric response for
+    // building attribute columns, for metric columns we can use other response
     PromQLMetricResponse firstResponse =
         promQLMetricResponseMap.values().stream().findFirst().get();
 
-    if (checkIfVectorResponse(firstResponse)) {
-      buildAggregateResponse(
+    if (isInstantResponse(firstResponse)) {
+      List<Builder> builderList = buildAggregateResponse(
           promQLMetricResponseMap,
           columnNameToAttributeMap,
           columnNameToQueryMap,
-          selectionList,
+          columnSet,
           firstResponse);
     } else {
-        // todo of timeseries
+      // todo of timeseries
+      buildAggregateResponse(promQLMetricResponseMap,
+          columnNameToAttributeMap,
+          columnNameToQueryMap,
+          columnSet,
+          firstResponse,
+          timeStampColumn);
     }
   }
 
@@ -48,20 +61,63 @@ public class PrometheusBasedResponseBuilder {
       Map<Request, PromQLMetricResponse> promQLMetricResponseMap,
       Map<String, String> columnNameToAttributeMap,
       Map<String, String> columnNameToQueryMap,
-      List<String> selectionList,
+      List<String> columnSet,
+      PromQLMetricResponse firstResponse,
+      String timeStampColumn) {
+
+    List<Builder> rowBuilderList = new ArrayList<>();
+    for (int row = 0; row < calcNumberRows(firstResponse); row++) {
+      // now time loop
+      for (int timeRow = 0; timeRow < firstResponse.getData().getResult().size(); timeRow++) {
+        Builder rowBuilder = Row.newBuilder();
+        PromQLMetricResult promQLMetricResult = firstResponse.getData().getResult().get(row);
+        // column loop
+
+        columnSet.forEach(
+            selection -> {
+              if (columnNameToQueryMap.containsKey(selection)) {
+                // metric selection
+                String colVal =
+                    getMetricValueForTime(
+                        columnNameToQueryMap.get(selection),
+                        promQLMetricResponseMap,
+                        promQLMetricResult);
+                rowBuilder.addColumn(Value.newBuilder().setString(colVal).build());
+              } else if (columnNameToAttributeMap.containsKey(selection)) {
+                // attribute selection
+                String colVal = getMetricAttributeValue(selection, promQLMetricResult);
+                rowBuilder.addColumn(Value.newBuilder().setString(colVal).build());
+              } else if (timeStampColumn.equals(selection)) {
+                // time stamp attribute
+                String colVal = getTimeValue();
+              } else {
+                throw new RuntimeException("Invalid selection");
+              }
+            });
+
+      }
+    }
+
+    return new ArrayList<>();
+
+  }
+  private static List<Builder> buildAggregateResponse(
+      Map<Request, PromQLMetricResponse> promQLMetricResponseMap,
+      Map<String, String> columnNameToAttributeMap,
+      Map<String, String> columnNameToQueryMap,
+      List<String> columnSet,
       PromQLMetricResponse firstResponse) {
+
     List<Builder> rowBuilderList = new ArrayList<>();
     for (int row = 0; row < calcNumberRows(firstResponse); row++) {
       Builder rowBuilder = Row.newBuilder();
-
       PromQLMetricResult promQLMetricResult = firstResponse.getData().getResult().get(row);
 
       // iterate over column and prepare a row
-      selectionList.forEach(
+      columnSet.forEach(
           selection -> {
-            // is metric selection
             if (columnNameToQueryMap.containsKey(selection)) {
-              // yes its metric value
+              // metric selection
               String colVal =
                   getMetricValue(
                       columnNameToQueryMap.get(selection),
@@ -69,7 +125,7 @@ public class PrometheusBasedResponseBuilder {
                       promQLMetricResult);
               rowBuilder.addColumn(Value.newBuilder().setString(colVal).build());
             } else if (columnNameToAttributeMap.containsKey(selection)) {
-              // yes, its attribute value
+              // attribute selection
               String colVal = getMetricAttributeValue(selection, promQLMetricResult);
               rowBuilder.addColumn(Value.newBuilder().setString(colVal).build());
             } else {
@@ -82,8 +138,13 @@ public class PrometheusBasedResponseBuilder {
     return rowBuilderList;
   }
 
-  private static boolean checkIfVectorResponse(PromQLMetricResponse firstResponse) {
+
+  private static boolean isInstantResponse(PromQLMetricResponse firstResponse) {
     return firstResponse.getData().getResultType().equals("vector");
+  }
+
+  private static boolean isRangeResponse(PromQLMetricResponse firstResponse) {
+    return firstResponse.getData().getResultType().equals("matrix");
   }
 
   private static long calcNumberRows(PromQLMetricResponse firstResponse) {
@@ -114,5 +175,33 @@ public class PrometheusBasedResponseBuilder {
   private static String getMetricAttributeValue(
       String selection, PromQLMetricResult promQLMetricResult) {
     return promQLMetricResult.getMetricAttributes().get(selection);
+  }
+
+  private static String getMetricValueForTime(
+      String query,
+      Map<Request, PromQLMetricResponse> promQLMetricResponseMap,
+      PromQLMetricResult promQLMetricResult,
+      Instant time) {
+    PromQLMetricResponse matchedResponse =
+        promQLMetricResponseMap.entrySet().stream()
+            .filter(entry -> entry.getKey().url().queryParameter("query").equals(query))
+            .findFirst()
+            .get()
+            .getValue();
+
+    return String.valueOf(
+        matchedResponse.getData().getResult().stream()
+            .filter(
+                metricResult ->
+                    metricResult
+                        .getMetricAttributes()
+                        .equals(promQLMetricResult.getMetricAttributes()))
+            .map(matchedResult -> matchedResult
+                .getValues()
+                .stream()
+                .filter(value -> value.getTimeStamp().equals(time))
+                .findFirst())
+            .filter(result -> result.isPresent())
+            .collect(Collectors.toUnmodifiableList()));
   }
 }
