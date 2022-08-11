7f4c94381453c5b35350576900a8d94916b3c9d7
Kshitiz Saxena <84707889+saxenakshitiz@users.noreply.github.com>
2022-08-11 23:44:43 +0530

    feat: add scalar functions in postgres (#165)
    - Implemented only conditional and stringEquals function based on projection function support required for QS(as per attribute service)

--- query-service/src/integrationTest/resources/sql/functions.sql
+++ query-service/src/integrationTest/resources/sql/functions.sql
@@ -1,1 +1,31 @@
 CREATE OR REPLACE FUNCTION dateTimeConvert (bigint, bigint) RETURNS bigint AS $$ select ((($1 + $2 - 1)/$2)*$2) $$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION conditional (condition text, first text, second text)
+ RETURNS text
+  AS $$
+   SELECT
+    CASE WHEN condition IS NOT NULL
+     THEN CASE WHEN CAST (condition AS BOOLEAN) THEN first ELSE second END
+     ELSE NULL
+    END
+  $$
+  LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION stringEquals (str1 text, str2 text)
+ RETURNS text
+  AS $$
+   SELECT
+    CASE WHEN str1 IS NULL AND str2 IS NULL
+     THEN 'true'
+     ELSE
+      CASE WHEN str1 IS NULL OR str2 IS NULL
+       THEN 'false'
+       ELSE
+        CASE WHEN str1 = str2
+         THEN 'true'
+         ELSE 'false'
+        END
+      END
+    END
+  $$
+  LANGUAGE SQL;
