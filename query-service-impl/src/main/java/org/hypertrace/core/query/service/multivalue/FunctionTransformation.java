43355a2cdccebf07b3717f4bd4c474db31fcbdd4
saxenakshitiz <kshitiz@traceable.ai>
2022-09-06 10:06:17 +0530

    Remove changes in FunctionTransformation

--- query-service-impl/src/main/java/org/hypertrace/core/query/service/multivalue/FunctionTransformation.java
+++ query-service-impl/src/main/java/org/hypertrace/core/query/service/multivalue/FunctionTransformation.java
@@ -8,20 +8,15 @@ import static org.hypertrace.core.attribute.service.v1.AttributeKind.TYPE_STRING
 import com.google.inject.Inject;
 import io.reactivex.rxjava3.core.Single;
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.hypertrace.core.attribute.service.cachingclient.CachingAttributeClient;
 import org.hypertrace.core.attribute.service.v1.AttributeKind;
-import org.hypertrace.core.attribute.service.v1.AttributeMetadata;
 import org.hypertrace.core.query.service.api.Function;
 
-@Slf4j
 public class FunctionTransformation {
   protected static final String DISTINCT_COUNT = "DISTINCTCOUNT";
   protected static final String DISTINCT_COUNT_MV = "DISTINCTCOUNTMV";
   private static final List<AttributeKind> ARRAY_KINDS =
       List.of(TYPE_STRING_ARRAY, TYPE_BOOL_ARRAY, TYPE_DOUBLE_ARRAY, TYPE_INT64_ARRAY);
-  private static final AttributeMetadata DEFAULT_ATTRIBUTE_METADATA =
-      AttributeMetadata.newBuilder().setValueKind(AttributeKind.TYPE_STRING).build();
   private final CachingAttributeClient attributeClient;
 
   @Inject
@@ -46,8 +41,6 @@ public class FunctionTransformation {
   private Single<Function.Builder> transformDistinctCount(Function.Builder functionBuilder) {
     return this.getFirstAttributeId(functionBuilder)
         .flatMap(this.attributeClient::get)
-        .doOnError(
-            error -> log.error("Unable to get resolve attribute using attribute service", error))
         .map(
             metadata ->
                 isArray(metadata.getValueKind())
