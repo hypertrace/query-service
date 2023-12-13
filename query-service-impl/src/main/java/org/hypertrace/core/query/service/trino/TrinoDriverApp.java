36b7cb83cee024e27ce3a811ad91a4a12a9980f7
satish <satish@traceable.ai>
2023-12-13 10:25:28 +0530

    create materialized view for results

--- query-service-impl/src/main/java/org/hypertrace/core/query/service/trino/TrinoDriverApp.java
+++ query-service-impl/src/main/java/org/hypertrace/core/query/service/trino/TrinoDriverApp.java
@@ -18,26 +18,50 @@ public class TrinoDriverApp {
     //properties.setProperty("SSL", "true");
     Connection connection = DriverManager.getConnection(url, properties);
     Statement statement = connection.createStatement();
+
+    long startTimeMillis = System.currentTimeMillis();
+    int updateCount =
+        statement.executeUpdate(
+            "CREATE MATERIALIZED VIEW IF NOT EXISTS per_api_span_count AS "
+                + "Select api_id, count(*) as count "
+                + "from span_event_view "
+                + "where customer_id = '3e761879-c77b-4d8f-a075-62ff28e8fa8a' "
+                + "and start_time_millis > 1698822000000 "
+                + "and regexp_like(request_body, '.*id.*') and regexp_like(response_body, '.*id.*') "
+                + "group by api_id order by count DESC limit 1000");
+    long endTimeMillis = System.currentTimeMillis();
+    System.out.printf("updateCount: %d time: %d\n", updateCount, (endTimeMillis - startTimeMillis)/1000);
+
+    startTimeMillis = System.currentTimeMillis();
+    int updateCount1 =
+        statement.executeUpdate("REFRESH MATERIALIZED VIEW per_api_span_count");
+    endTimeMillis = System.currentTimeMillis();
+    System.out.printf("updateCount1: %d time: %d\n", updateCount1, (endTimeMillis - startTimeMillis)/1000);
+
+    // fetch results twice from view
+    getResult(statement);
+    getResult(statement);
+  }
+
+  private static void getResult(Statement statement) throws SQLException {
+    long startTimeMillis = System.currentTimeMillis();
     ResultSet resultSet =
-        statement.executeQuery("Select api_id, api_name, service_id, service_name, COUNT(*) as count "
-            + "from span_event_view_staging_test "
-            + "where customer_id = 'b227d0f9-98e1-4eff-acf5-ab129d416914' "
-            + "and start_time_millis >= 1692943200000  and start_time_millis < 1692946800000 "
-            + "AND api_id != 'null' AND api_discovery_state IN ('DISCOVERED', 'UNDER_DISCOVERY') "
-            + "GROUP BY api_id, api_name, service_name, service_id limit 10000");
+        statement.executeQuery("Select api_id, count from per_api_span_count limit 10");
+    long endTimeMillis = System.currentTimeMillis();
 
     String api_id, api_name, service_name, service_id = null;
     int count = 0;
     int total = 0;
     while (resultSet.next()) {
       api_id = resultSet.getString("api_id");
-      api_name = resultSet.getString("api_name");
-      service_id = resultSet.getString("service_id");
-      service_name = resultSet.getString("service_name");
+//      api_name = resultSet.getString("api_name");
+//      service_id = resultSet.getString("service_id");
+//      service_name = resultSet.getString("service_name");
       count = resultSet.getInt("count");
-      System.out.println(String.format("%s, %s, %s, %s, %d", api_id, api_name, service_id, service_name, count));
+      //          String.format("%s, %s, %s, %s, %d", api_id, api_name, service_id, service_name, count));
+      System.out.printf("api_id: %s count: %d%n", api_id, count);
       total++;
     }
-    System.out.println("total: " + total);
+    System.out.printf("total rows: %d time: %d\n", total, (endTimeMillis - startTimeMillis)/1000);
   }
 }
